6b0179d2d9195e822b3eaced181600335736065e
Lcbzinho <bassanilucas055@gmail.com>
2026-01-06 00:23:01 -0300

    feat: funções TypeScript e configuração de build

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function multiply(n1, n2) {
+    return n1 * n2;
+}
+function greeting(name) {
+    return `Olá ${name}`;
+}
+console.log(multiply(10, 10));
+console.log(greeting("Gui"));
+//# sourceMappingURL=main.js.map
--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
-function multiply(n1:number, n2:number) {
-    return (n1*n2)
-};
+function multiply(n1: number, n2: number) {
+    return n1 * n2;
+}
 
-function greeting(name:string) {
-    return `Hello ${name}`;
-};
+function greeting(name: string) {
+    return `Olá ${name}`;
+}
 
-console.log(multiply(10,10));
-console.log(greeting("Gui"))
+console.log(multiply(10, 10));
+console.log(greeting("Gui"));
